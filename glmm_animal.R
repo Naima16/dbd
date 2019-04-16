7645ef1a41cf56cd180339b41595f99c4560d921
Naima16 <madi671@hotmail.com>
2019-04-16 08:38:33 -0400

    Update glmm_animal.R

--- glmm_animal.R
+++ glmm_animal.R
@@ -68,9 +68,11 @@ for (i in 1: dim(df.animales)[1]) {
   if (df.animales[i,'genus_type'] %in% unique(animales$genus_type) )
       df.animales[i,'status'] = "Native"
   else
-      df.animales[i,'status'] = "Migrant"
+    if (df.animales[i,'genus_type'] %in% unique(Saline$genus_type) || df.animales[i,'genus_type'] %in% unique(nonsaline$genus_type))
+        df.animales[i,'status'] = "Migrant"
+    else
+      df.animales[i,'status'] = "Generalist"
 }
-
 df.animales$status=as.factor(df.animales$status)
 
 ################
@@ -84,22 +86,24 @@ save(datsc_animal,file="datsc_animal.RData")
 
 
 ########################### Full glmm
-glmer.full.status.1 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+ (nb_genus|empo_3/PI)+(nb_genus|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
+glmm.animal.1 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+ (nb_genus|empo_3/PI)+(nb_genus|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
 
-summary(glmer.full.status.1)
-save(glmer.full.status.1,file="glmm_animal1.RData")
+# summary(glmer.full.status.1)
+ save(glmm.animal.1,file="glmm_animal1.RData")
 
 ###enlever  singularité sur sample et empo_3 car cor=1
-glmer.full.2 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+(nb_genus-1|empo_3)+ (nb_genus|empo_3:PI)+(nb_genus-1|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
-save(glmer.full.2,file="glmm_animal2.RData")
+glmm.animal.2 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+(nb_genus-1|empo_3)+ (nb_genus|empo_3:PI)+(nb_genus-1|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
+save(glmm.animal.2,file="glmm_animal2.RData")
 
 ###enlever empo3 car 0 (et le plus significant)
-glmer.full.3 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+ (nb_genus|empo_3:PI)+(nb_genus-1|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
-save(glmer.full.3,file="glmm_animal3.RData")
-summary(glmer.full.3)
+glmm.anim.3 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+ (nb_genus|empo_3:PI)+(nb_genus-1|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
+save(glmm.anim.3,file="glmm_animal3.RData")
+summary(glmm.anim.3)
 
 
-### random effect significance
+###significance
+glmm.anim.3 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+ (nb_genus|empo_3:PI)+(nb_genus-1|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
+
 glmer.full.4 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3)+ (nb_genus|empo_3:PI),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
 
 glmer.full.5 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type/empo_3),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
@@ -108,32 +112,35 @@ glmer.full.6 = glmer(nb_ASV~nb_genus*status+(nb_genus|genus_type:empo_3),datsc1,
 
 glmer.full.7 = lm(nb_ASV~nb_genus*status,datsc1)
 
-anova(glmer.full.3,glmer.full.4,glmer.full.5,glmer.full.6,glmer.full.7)
-#   glmer.full.7  5 136317 136359 -68153   136307                                
-#   glmer.full.6  7 100005 100064 -49996    99991 36315.902      2  < 2.2e-16 ***
-#   glmer.full.5 10  99338  99423 -49659    99318   672.572      3  < 2.2e-16 ***
-#   glmer.full.4 13  98770  98879 -49372    98744   574.914      3  < 2.2e-16 ***
-#   glmer.full.3 14  98717  98835 -49345    98689    54.386      1  1.647e-13 ***
+anova(glmm.anim.3,glmer.full.4,glmer.full.5,glmer.full.6,glmer.full.7)
+# Df    AIC    BIC logLik deviance     Chisq Chi Df Pr(>Chisq)    
+# glmer.full.7  7 136314 136373 -68150   136300                                
+# glmer.full.6  9 100007 100083 -49995    99989 36310.980      2  < 2.2e-16 ***
+#   glmer.full.5 12  99341  99442 -49658    99317   672.584      3  < 2.2e-16 ***
+#   glmer.full.4 15  98770  98896 -49370    98740   576.606      3  < 2.2e-16 ***
+#   glmm.anim.3  16  98718  98853 -49343    98686    53.733      1  2.297e-13 ***
 
 ###tester la significance de l'interaction
 glmer.full.31 = glmer(nb_ASV~nb_genus+status+(nb_genus|genus_type/empo_3)+ (nb_genus|empo_3:PI)+(nb_genus-1|sample),datsc1,family=poisson(link=log),control=glmerControl(optimizer="bobyqa"))
-anova(glmer.full.31,glmer.full.3)
+anova(glmm.anim.3,glmer.full.31)
 # Df   AIC   BIC logLik deviance Chisq Chi Df Pr(>Chisq)    
-# glmer.full.31 13 98771 98881 -49373    98745                            
-# glmer.full.3  14 98717 98835 -49345    98689 56.01      1   7.21e-14 ***
+# glmer.full.31 14 98771 98889 -49371    98743                             
+# glmm.anim.3   16 98718 98853 -49343    98686 56.273      2  6.031e-13 ***
 
+## plot the interaction
 require(sjPlot)
-pdf("animal.pdf")
-plot1=plot_model(glmer.full.3,type="int",terms=c("nb_genus","status"),title="Animal",show.legend = TRUE, axis.title=c("","ASV number/genus"),colors = c("black","red"))
+pdf("animal3status.pdf")
+plot1=plot_model(glmm.anim.3,type="int",terms=c("nb_genus","status"),title="Animal",show.legend = TRUE, axis.title=c("","ASV number/genus"),colors = c("black","red","orange"))
 plot1
 dev.off()
 
-# ###
-overdisp_fun(glmer.full.3)
+####
+overdisp_fun(glmm.anim.3)
 # chisq        ratio          rdf            p 
 # 1.756283e+04 5.274597e-01 3.329700e+04 1.000000e+00
 
+
 ## diagnostic plots
-plot(fitted(glmer.full.3),residuals(glmer.full.3),xlab = "Fitted Values", ylab = "Residuals")
-qqnorm(scale(resid(glmer.full.3)),ylab="Residual quantiles",col="orange")
+plot(fitted(glmm.anim.3),residuals(glmm.anim.3),xlab = "Fitted Values", ylab = "Residuals")
+qqnorm(scale(resid(glmm.anim.3)),ylab="Residual quantiles",col="orange")
 
