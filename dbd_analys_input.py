f5060104b3ff48edc3ebc8abbff1cb00326c2a0c
Naima16 <madi671@hotmail.com>
2019-11-23 09:16:20 -0500

    Update dbd_analys_input.py

--- dbd_analys_input.py
+++ dbd_analys_input.py
@@ -8,19 +8,20 @@ import numpy  as np
 
 def list_sample(df, index):
     return(set([x for x in df.loc[index]['list_samples'].split(',')]))
-  
-path_otus = '/Users/naima/aout8_2018/test/otu_summary.emp_deblur_90bp.subset_2k.rare_5000.tsv'
+
+## summary file from EMP ftp site
+path_otus = '~/otu_summary.emp_deblur_90bp.subset_2k.rare_5000.tsv'
 df_otus = pd.read_csv(path_otus, sep='\t', index_col=0)
 df_otus['samples'] = [list_sample(df_otus, index) for index in df_otus.index]
 
 liste_seq=pd.DataFrame(columns=['sample','taxonomy'])
 df_otus.samples.str
-idx = 7 ##taxonomy
+idx=df_otus.columns.get_loc("taxonomy") ##7 
+idsample=df_otus.columns.get_loc("samples") ##9
 
 for i in range(0,len(df_otus)):
-  ######indice 9 : samples
- if (df_otus.iloc[i,9] != None):
-   for element in df_otus.iloc[i,9] :
+ if (df_otus.iloc[i,idsample] != None):
+   for element in df_otus.iloc[i,idsample] :
       indx1=liste_seq.index[liste_seq['sample']==element]       
       if ( indx1 != None) :
           liste_seq.loc[indx1,'taxonomy']=liste_seq.loc[indx1,'taxonomy'] + ","+ df_otus.iloc[i,idx]
